4cd4cf89eff940b218d3fc623ebd3c7aafafc459
iKoloser <162134363+iKoloser@users.noreply.github.com>
2024-04-17 15:56:25 +0200

    Done

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
 "use strict";
 class Book {
+    constructor(title) {
+        this.title = title;
+    }
+    read() {
+    }
 }
-class PaperBook {
+class PaperBook extends Book {
+    constructor(title, pages) {
+        super(title);
+        this.pages = pages;
+    }
+    read() {
+        return "Book in papel";
+    }
 }
-class ElectronicBook {
+class ElectronicBook extends Book {
+    constructor(title, size) {
+        super(title);
+        this.size = size;
+    }
+    read() {
+        return "Book in digital";
+    }
 }
-class ParchmentBook {
+class ParchmentBook extends Book {
+    constructor(title) {
+        super(title);
+    }
+    read() {
+        return "Book in Parchment";
+    }
 }
 class ReadBook {
+    constructor(books) {
+        this.books = books;
+    }
+    read() {
+        return this.books.read();
+    }
 }
 const phisicalBook = new PaperBook("Dragones de papel", 350);
 const digitalBook = new ElectronicBook("One piece", 150);
+const parchmenttBook = new ParchmentBook("Soy Leyenda");
 const readerPaper = new ReadBook(phisicalBook);
 const readerDigital = new ReadBook(digitalBook);
+const readerParchment = new ReadBook(parchmenttBook);
 console.log(readerDigital.read());
 console.log(readerPaper.read());
+console.log(readerParchment.read());
--- app.ts
+++ app.ts
@@ -1,18 +1,67 @@
-class Book{}
+class Book{
+    title:string;
+    constructor(title:string){
+        this.title = title;
+    }
+    read(){
+        
+    }
+}
 
-class PaperBook{}
+class PaperBook extends Book{
+    pages:number;
+    constructor(title:string, pages:number){
+        super(title);
+        this.pages = pages;
+    }
 
-class ElectronicBook{}
+    read() {
+        return "Book in papel"
+    }
+}
 
-class ParchmentBook{}
+class ElectronicBook extends Book{
+    size:number;
+    constructor(title:string, size:number){
+        super(title);
+        this.size = size;
+    }
+    read() {
+        return "Book in digital"
+    }
+}
 
-class ReadBook{}
+class ParchmentBook extends Book{
+    constructor(title:string){
+        super(title);
+    }
+    read() {
+        return "Book in Parchment"
+    }
+}
+
+class ReadBook{
+
+
+    books: Book;
+    constructor(books:Book){
+        this.books = books;
+    }
+
+    read() {
+        return this.books.read();
+    } 
+}
 
 
 const phisicalBook = new PaperBook("Dragones de papel",350);
 const digitalBook = new ElectronicBook("One piece",150);
+const parchmenttBook = new ParchmentBook("Soy Leyenda");
+
 const readerPaper = new ReadBook(phisicalBook);
 const readerDigital= new ReadBook(digitalBook);
+const readerParchment= new ReadBook(parchmenttBook);
 
 console.log (readerDigital.read());
 console.log(readerPaper.read());
+console.log(readerParchment.read())
